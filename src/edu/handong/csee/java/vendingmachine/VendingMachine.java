fb2db5840c9f6e3f5d21a0d69356b568ed9dcb6e
bbibbd <bbibochaa74@gmail.com>
2018-10-23 21:50:34 +0300

    add javadoc

--- src/edu/handong/csee/java/vendingmachine/VendingMachine.java
+++ src/edu/handong/csee/java/vendingmachine/VendingMachine.java
@@ -26,6 +26,9 @@ public class VendingMachine {
 		System.out.println("1. Coca cola: 1000Won\n2. Pepsi cola: 900Won\n3. sprite: 1100Won\n4. Black coffee: 500Won\n5. White_coffee: 600Won\nChoose the product you want: ");
 	}
 
+	/**this method gets choice from user and set it to the vending machine.
+	 * Each choices sets product and price
+	*/
 	public void settingProduct(int choice) {
 		if(choice == 1) {
 			product = "Coca cola";
@@ -49,6 +52,7 @@ public class VendingMachine {
 		}
 	}
 
+	
 	public int returnChange() {
 		System.out.printf("inserted %d won \n", coin);    
 		if(coin>=price){
